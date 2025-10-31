3ade23ea4645d73d4797f0ebfee42841a3d4b00f
Piyush Yadav <piyushyadav20045@gmail.com>
2025-10-31 23:59:36 +0530

    ui: add a separate component for title creation

---
+++ app/_components/logo-title.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { PenLine, Shapes } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+
+// LogoTitle component
+const LogoTitle = () => {
+  const searchParams = useSearchParams();
+  const [title, setTitle] = useState(searchParams.get("title") || "");
+
+  return (
+    <div className="mx-2 mt-24 p-4 md:p-6 border rounded-2xl bg-blue-100 max-w-lg shadow-lg shadow-black/10">
+      {/* Title */}
+      <h1 className="text-2xl md:text-3xl font-bold text-blue-600 flex items-center gap-1">
+        <Shapes />
+        Enter a logo name
+      </h1>
+      <p className="mt-1 text-lg font-semibold text-gray-700">
+        Enter your web app name or company name:
+      </p>
+      {/* Input and Button */}
+      <form>
+        <Input
+          type="text"
+          placeholder="Enter a logo name"
+          className="h-12 mt-8 bg-white text-lg! font-semibold text-gray-800 shadow-md shadow-black/10 placeholder:text-gray-400 focus:ring-blue-200!"
+        />
+        <Button className="h-12 mt-4 w-full bg-linear-to-tl from-blue-400 via-slate-600 to-blue-400 text-lg shadow-md shadow-black/20 transition-all duration-300 active:scale-95 cursor-pointer">
+          Create Logo <PenLine />
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default LogoTitle;
--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,36 +1,12 @@
 "use client";
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { PenLine, Shapes } from "lucide-react";
-import { useSearchParams } from "next/navigation";
+import LogoTitle from "../_components/logo-title";
 
 // Create page component
 const Create = () => {
-  const title = useSearchParams().get("title") || "";
   return (
     <div className="bg-[url('/bg.webp')] bg-center bg-cover flex flex-col items-center">
-      <div className="mx-2 mt-24 p-4 md:p-6 border rounded-2xl bg-blue-100 max-w-lg shadow-lg shadow-black/10">
-        {/* Title */}
-        <h1 className="text-2xl md:text-3xl font-bold text-blue-600 flex items-center gap-1">
-          <Shapes />
-          Enter a logo name
-        </h1>
-        <p className="mt-1 text-lg font-semibold text-gray-700">
-          Enter your web app name or company name:
-        </p>
-        {/* Input and Button */}
-        <form>
-          <Input
-            type="text"
-            placeholder="Enter a logo name"
-            className="h-12 mt-8 bg-white text-lg! font-semibold text-gray-800 shadow-md shadow-black/10 placeholder:text-gray-400 focus:ring-blue-200!"
-          />
-          <Button className="h-12 mt-4 w-full bg-linear-to-tl from-blue-400 via-slate-600 to-blue-400 text-lg shadow-md shadow-black/20 transition-all duration-300 active:scale-95 cursor-pointer">
-            Create Logo <PenLine />
-          </Button>
-        </form>
-      </div>
+      <LogoTitle />
     </div>
   );
 };
