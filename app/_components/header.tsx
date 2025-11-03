9cf36e5ea3543ac6499cce0fd8602ddc0b67681c
Piyush Yadav <piyushyadav20045@gmail.com>
2025-11-03 23:54:31 +0530

    feat(header): make logo clickable and redirect to home

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { LogIn } from "lucide-react";
+import Link from "next/link";
 
 // Header Component
 const header = () => {
   return (
     <div className="px-3 md:px-20 2xl:px-56 py-2 flex justify-between items-center shadow-sm">
       {/* LOGO */}
-      <div className="flex items-center cursor-pointer">
+      <Link href="/" className="flex items-center cursor-pointer">
         <Image width={55} height={55} src="/logo.png" alt="logo" />
         <span className="font-bold text-3xl bg-linear-to-tl from-blue-400 via-slate-600 to-blue-400 bg-clip-text text-transparent">
           logofy
         </span>
-      </div>
+      </Link>
 
       {/* NAV LINKS */}
       <Button className="custom-button text-lg">
