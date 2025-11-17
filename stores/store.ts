11b6fcb6a1a625e425a8de2dd2c4cebc61f36133
Piyush Yadav <piyushyadav20045@gmail.com>
2025-11-17 19:50:54 +0530

    feat(store): add persisted formData to zustand store

--- stores/store.ts
+++ stores/store.ts
@@ -1,13 +1,44 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface CreateFormData {
+  title?: string;
+  desc?: string;
+  palette?: string;
+  design?: {
+    image: string;
+    prompt: string;
+    title: string;
+  };
+}
 
 interface StoreState {
   step: number;
   incStep: (step: number) => void;
   decStep: (step: number) => void;
+  formData: CreateFormData;
+  setFormData: (field: string, value: string | object) => void;
+  clearFormData: () => void;
 }
 
-export const useStore = create<StoreState>((set) => ({
-  step: 1,
-  incStep: () => set((state) => ({ step: state.step + 1 })),
-  decStep: () => set((state) => ({ step: state.step - 1 })),
-}));
+export const useStore = create<StoreState>()(
+  persist(
+    (set) => ({
+      step: 1,
+      incStep: () => set((state) => ({ step: state.step + 1 })),
+      decStep: () => set((state) => ({ step: state.step - 1 })),
+      formData: {},
+      setFormData: (field: string, value: string | object) =>
+        set((state) => ({
+          formData: { ...state.formData, [field]: value },
+        })),
+      clearFormData: () => set({ formData: {} }),
+    }),
+    {
+      name: "formData",
+      partialize: (state) => ({
+        formData: state.formData,
+      }),
+    }
+  )
+);
